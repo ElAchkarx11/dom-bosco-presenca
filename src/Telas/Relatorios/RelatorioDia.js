ea555847f3a54f8312483266dfb2793e6e738d16
Roberto Naim J. El Achkar Junior <78863403+ElAchkarx11@users.noreply.github.com>
2024-06-12 22:05:58 -0300

    Add files via upload

--- src/Telas/Relatorios/RelatorioDia.js
+++ src/Telas/Relatorios/RelatorioDia.js
@@ -5,7 +5,6 @@ import { signOut, getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useNavigate, useParams } from 'react-router-dom';
 import { BiDoorOpen, BiArrowBack } from 'react-icons/bi';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { addDays } from 'date-fns'; // Importa a função addDays do date-fns
 
 function RelatorioDia() {
     const { dia } = useParams();
@@ -52,29 +51,20 @@ function RelatorioDia() {
         fetchData();
     }, [dia]);
 
-    const diaMaisUm = addDays(new Date(parseInt(dia)), 1); // Adiciona um dia à data
-
     return (
-        <>
-
-            <div style={{ display: 'flex', backgroundColor: '#273585', width: '100vw', height: '100vh', alignItems: 'center', alignContent: 'center', flexDirection: 'column' }}>
-
-
-
-
+        <div style={{ display: 'flex', backgroundColor: '#273585', width: '100vw', height: '100vh', alignItems: 'center', alignContent: 'center', flexDirection: 'column' }}>
+            <div style={{ width: '90%', textAlign: 'center', height: '60%' }}>
                 <div style={{ display: 'flex', color: '#FFF', fontWeight: 'bold', marginBottom: 16, fontSize: 26, flexDirection: 'row', width: '100%', alignItems: 'center' }}>
                     <BiArrowBack style={{ width: '10%' }} size={26} onClick={handleBack} />
-
-                    Relatório do dia {diaMaisUm.toLocaleDateString('pt-BR')}
+                    Relatório do dia {dataFormatada} {/* Utiliza dataFormatada diretamente para exibir o dia correto */}
                 </div>
-
                 <div style={{ backgroundColor: '#FFF', padding: 20, borderRadius: 5 }}>
                     <div className="row">
                         {entradas.length > 0 ?
                             entradas.map((entrada, index) => {
                                 return (
                                     <div
-                                        style={{ border: '2px solid #273585', borderRadius: 5, marginBottom: 8,padding: 5, backgroundColor: '#FFF', fontWeight: 'bold' }}
+                                        style={{ border: '2px solid #273585', borderRadius: 5, marginBottom: 8, padding: 5, backgroundColor: '#FFF', fontWeight: 'bold' }}
                                         key={index}
                                     >
                                         <h5 className="mb-1">{entrada.nome}</h5>
@@ -83,45 +73,13 @@ function RelatorioDia() {
                                     </div>
                                 );
                             })
-                            : (<>
+                            : (
                                 <div style={{ fontWeight: 'bold' }}>
                                     Não há dados.
                                 </div>
-                            </>)}
-
-
+                            )}
                     </div>
                 </div>
-            </div>
-
-        </>
-    );
-
-    return (
-        <div className="container">
-            <div className='header row'>
-                <div className='p-4 col-6 text-start'>
-                    <button className='btn border-none ' onClick={handleBack}>
-                        <BiArrowBack className='' style={{ fontSize: "30px" }} />
-                    </button>
-                </div>
-                <div className='p-4 col-6 text-end'>
-                    <button className='btn border-none ' onClick={handleLogout}>
-                        <BiDoorOpen className='' style={{ fontSize: "30px" }} />
-                    </button>
-                </div>
-                <h1 className="text-center my-4">Relatório do Dia: {diaMaisUm.toLocaleDateString('pt-BR')}</h1> {/* Exibe o dia + 1 */}
-                <hr />
-            </div>
-
-            <div className="list-group">
-                {entradas.map((entrada, index) => (
-                    <div className="list-group-item" key={index}>
-                        <h5 className="mb-1">{entrada.nome}</h5>
-                        <p className="mb-1">CPF: {entrada.cpf}</p>
-                        <small>Timestamp: {entrada.timestamp && entrada.timestamp.toDate && entrada.timestamp.toDate().toLocaleString('pt-BR')}</small>
-                    </div>
-                ))}
             </div>
         </div>
     );
--- src/Telas/Relatorios/Relatorios.js
+++ src/Telas/Relatorios/Relatorios.js
@@ -1,19 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { collection, getDocs, query, orderBy, where, and } from 'firebase/firestore';
+import { collection, getDocs, query, orderBy, where } from 'firebase/firestore';
 import { db, auth } from '../../firebase';
 import { signOut, getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BiSortAlt2, BiArrowBack } from 'react-icons/bi';
-import { format, startOfDay, addDays } from 'date-fns';
-import { toZonedTime } from 'date-fns-tz';
+import { startOfDay, endOfDay, addDays } from 'date-fns';
 
 function Relatorios() {
     const [dias, setDias] = useState([]);
     const navigate = useNavigate();
     const [authenticated, setAuthenticated] = useState(false);
-    const [dateCalendar, setDateCalendar] = useState(new Date());
-    const [dateCalendarFinal, setDateCalendarFinal] = useState(new Date(new Date().setDate(new Date().getDate() - 1)));
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
     const [order, setOrder] = useState('desc');
 
     useEffect(() => {
@@ -37,90 +36,98 @@ function Relatorios() {
         navigate(-1);
     };
 
-    useEffect(() => {
-        const fetchData = async () => {
+    const fetchRelatorios = async () => {
+        if (!startDate || !endDate) {
+            console.error("Por favor, selecione o intervalo de datas.");
+            return;
+        }
 
-            const q = query(collection(db, 'entradas'), and(where('timestamp', '<=', dateCalendar), where('timestamp', '>=', dateCalendarFinal)), orderBy('timestamp', order));
+        const inicio = startOfDay(new Date(startDate));
+        const fim = endOfDay(new Date(endDate));
+
+        try {
+            const q = query(
+                collection(db, 'entradas'),
+                where('timestamp', '>=', inicio),
+                where('timestamp', '<=', fim),
+                orderBy('timestamp', order)
+            );
+
             const querySnapshot = await getDocs(q);
             const diasMap = new Map();
-            const timeZone = 'America/Sao_Paulo'; // Defina o fuso horário correto
 
             querySnapshot.forEach(doc => {
                 const data = doc.data();
                 if (data.timestamp && data.timestamp.toDate) {
                     const timestampDate = data.timestamp.toDate();
-                    const zonedDate = toZonedTime(timestampDate, timeZone); // Converte para o fuso horário correto
-                    const dateKey = format(startOfDay(zonedDate), 'yyyy-MM-dd');
+                    const dateKey = timestampDate.toISOString().split('T')[0];
                     if (!diasMap.has(dateKey)) {
-                        diasMap.set(dateKey, new Date(dateKey));
+                        diasMap.set(dateKey, timestampDate);
                     }
                 }
             });
             setDias(Array.from(diasMap.values()));
-        };
-        fetchData();
-    }, [dateCalendar, dateCalendarFinal, order]);
+        } catch (error) {
+            console.error("Erro ao buscar relatórios: ", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchRelatorios();
+    }, [startDate, endDate, order]);
 
     const handleDiaClick = (dia) => {
         navigate(`/RelatorioDia/${dia.getTime()}`);
     };
 
     return (
-        <>
-
-            <div style={{ display: 'flex', backgroundColor: '#273585', width: '100vw', height: '100vh', alignItems: 'center', alignContent: 'center', flexDirection: 'column' }}>
-
-                <div style={{ width: '90%', textAlign: 'center', height: '60%' }}>
-
-
-
-                    <div style={{ display: 'flex', color: '#FFF', fontWeight: 'bold', marginBottom: 16, fontSize: 26, flexDirection: 'row', width: '100%', alignItems: 'center' }}>
-                        <BiArrowBack style={{ width: '10%' }} size={26} onClick={handleBack} />
-
-                        Relatórios
-                    </div>
-
-                    <div style={{ backgroundColor: '#FFF', padding: 20, borderRadius: 5, marginBottom: 8, justifyContent: 'space-evenly', display: 'flex', alignItems: 'center' }}>
-                        De
-                        <input type="date" value={format(startOfDay(dateCalendarFinal), 'yyyy-MM-dd')} onChange={(e) => { setDateCalendarFinal(new Date(e.target.value)) }} style={{ border: '2px solid #273585', height: '100%', borderRadius: 5, backgroundColor: '#FFF', padding: 5 }}
-                        />
-                        até
-                        <input type="date" value={format(startOfDay(dateCalendar), 'yyyy-MM-dd')} onChange={(e) => { setDateCalendar(new Date(e.target.value)) }} style={{ border: '2px solid #273585', height: '100%', borderRadius: 5, backgroundColor: '#FFF', padding: 5 }}
-                        />
-
-                        <BiSortAlt2 style={{ width: '10%' }} size={26} onClick={() => {setOrder((curr) => curr === 'asc' ? 'desc': 'asc')}} />
-
-                    </div>
-
-                    <div style={{ backgroundColor: '#FFF', padding: 20, borderRadius: 5 }}>
-                        <div className="row">
-                            {dias.length > 0 ?
-                                dias.map((dia, index) => {
-                                    const diaMaisUm = addDays(dia, 1); // Adiciona um dia à data
-                                    return (
-                                        <button
-                                            style={{ border: '2px solid #273585', borderRadius: 5, marginBottom: 8, height: 50, alignContent: 'center', justifyContent: 'center', backgroundColor: '#FFF', fontWeight: 'bold' }}
-                                            key={index}
-                                            onClick={() => handleDiaClick(dia)}
-                                        >
-                                            {dia.toLocaleDateString('pt-BR')}
-                                        </button>
-                                    );
-                                })
-                                : (<>
-                                    <div style={{ fontWeight: 'bold' }}>
-                                        Não há dados.
-                                    </div>
-                                </>)}
-
-                        </div>
+        <div style={{ display: 'flex', backgroundColor: '#273585', width: '100vw', height: '100vh', alignItems: 'center', alignContent: 'center', flexDirection: 'column' }}>
+            <div style={{ width: '90%', textAlign: 'center', height: '60%'}}>
+                <div style={{ display: 'flex', color: '#FFF', fontWeight: 'bold', marginBottom: 16, fontSize: 26, flexDirection: 'row', width: '100%', alignItems: 'center' }}>
+                    <BiArrowBack style={{ width: '10%' }} size={26} onClick={handleBack} />
+                    Relatórios
+                </div>
+                <div style={{ backgroundColor: '#FFF', padding: 20, borderRadius: 5, marginBottom: 8, justifyContent: 'space-evenly', display: 'flex', alignItems: 'center' }}>
+                    De
+                    <input
+                        type="date"
+                        value={startDate.toISOString().split('T')[0]}
+                        onChange={(e) => setStartDate(new Date(e.target.value))}
+                        style={{ border: '2px solid #273585', height: '100%', borderRadius: 5, backgroundColor: '#FFF', padding: 5 }}
+                    />
+                    até
+                    <input
+                        type="date"
+                        value={endDate.toISOString().split('T')[0]}
+                        onChange={(e) => setEndDate(new Date(e.target.value))}
+                        style={{ border: '2px solid #273585', height: '100%', borderRadius: 5, backgroundColor: '#FFF', padding: 5 }}
+                    />
+                    <BiSortAlt2 style={{ width: '10%' }} size={26} onClick={() => setOrder(curr => curr === 'asc' ? 'desc' : 'asc')} />
+                </div>
+                <div style={{ backgroundColor: '#FFF', padding: 20, borderRadius: 5, height:'400px', overflowY: 'auto' }}>
+                    <div className="row">
+                        {dias.length > 0 ?
+                            dias.map((dia, index) => {
+                                return (
+                                    <button
+                                        style={{ border: '2px solid #273585', borderRadius: 5, marginBottom: 8, height: 50, alignContent: 'center', justifyContent: 'center', backgroundColor: '#FFF', fontWeight: 'bold' }}
+                                        key={index}
+                                        onClick={() => handleDiaClick(dia)}
+                                    >
+                                        {dia.toLocaleDateString('pt-BR')}
+                                    </button>
+                                );
+                            })
+                            : (
+                                <div style={{ fontWeight: 'bold' }}>
+                                    Não há dados.
+                                </div>
+                            )}
                     </div>
                 </div>
-
             </div>
-        </>
+        </div>
     );
-
 }
 
 export default Relatorios;
